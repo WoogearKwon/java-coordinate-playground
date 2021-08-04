597f79699f3ebf889c2a2cde1968f3ad55ecb520
Woogear Kwon <techkwon1717@hanmail.net>
2021-08-04 23:35:15 +0900

    [test] 필요한 연료량 테스트 추가

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -10,15 +10,18 @@ public class CarTest {
         assertThat(sonata.getName()).isEqualTo("Sonata");
         assertThat(sonata.getDistancePerLiter()).isEqualTo(10);
         assertThat(sonata.getTripDistance()).isEqualTo(100);
+        assertThat(sonata.getChargeQuantity()).isEqualTo(10);
 
-        Avante avante = new Avante(120);
+        Avante avante = new Avante(60);
         assertThat(avante.getName()).isEqualTo("Avante");
         assertThat(avante.getDistancePerLiter()).isEqualTo(15);
-        assertThat(avante.getTripDistance()).isEqualTo(120);
+        assertThat(avante.getTripDistance()).isEqualTo(60);
+        assertThat(avante.getChargeQuantity()).isEqualTo(4);
 
-        K5 k5 = new K5(210);
+        K5 k5 = new K5(260);
         assertThat(k5.getName()).isEqualTo("K5");
         assertThat(k5.getDistancePerLiter()).isEqualTo(13);
-        assertThat(k5.getTripDistance()).isEqualTo(210);
+        assertThat(k5.getTripDistance()).isEqualTo(260);
+        assertThat(k5.getChargeQuantity()).isEqualTo(20);
     }
 }
